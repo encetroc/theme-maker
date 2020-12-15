c877659f7b4949db052317ce982c699b077ad1a1
el mehdi rhindi <g6handi@gmail.com>
2020-12-15 20:55:11 +0100

    file formatting issues

--- src/Section/Section.test.js
+++ src/Section/Section.test.js
@@ -3,27 +3,27 @@ import { render } from '@testing-library/react'
 import Section from './Section'
 
 const mockSection = [
-    'colors',
-    {
-        primary: { value: '#000000', description: 'Primary font color' },
-        secondary: { value: '#ffffff', description: 'Secondary font color' }
-    }
+  'colors',
+  {
+    primary: { value: '#000000', description: 'Primary font color' },
+    secondary: { value: '#ffffff', description: 'Secondary font color' }
+  }
 ]
 
 it('Renders correctly', () => {
-    const { queryByTestId, queryAllByTestId } = render(<Section />)
-    expect(queryByTestId('section-title')).toBeTruthy()
-    expect(queryAllByTestId('style-picker')).toBeTruthy()
+  const { queryByTestId, queryAllByTestId } = render(<Section />)
+  expect(queryByTestId('section-title')).toBeTruthy()
+  expect(queryAllByTestId('style-picker')).toBeTruthy()
 })
 
 describe('Section content', () => {
-    it('Has the correct title', () => {
-        const { queryByTestId } = render(<Section section={mockSection} />)
-        expect(queryByTestId('section-title')).toHaveTextContent('colors')
-    })
+  it('Has the correct title', () => {
+    const { queryByTestId } = render(<Section section={mockSection} />)
+    expect(queryByTestId('section-title')).toHaveTextContent('colors')
+  })
 
-    it('Has the correct number of style pickers', () => {
-        const { queryAllByTestId } = render(<Section section={mockSection} />)
-        expect(queryAllByTestId('style-picker')).toHaveLength(2)
-    })
+  it('Has the correct number of style pickers', () => {
+    const { queryAllByTestId } = render(<Section section={mockSection} />)
+    expect(queryAllByTestId('style-picker')).toHaveLength(2)
+  })
 })
