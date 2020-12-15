1e3746ff6b043a8b85ec10399d8955cb689102be
el mehdi rhindi <g6handi@gmail.com>
2020-12-15 20:40:25 +0100

    added tests for App

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ it('Renders correctly', () => {
   const { queryByTestId } = render(<App />)
   expect(queryByTestId('save-btn')).toBeTruthy()
 })
+
+it('Saves theme data to local storage', () => {
+  const { queryByTestId } = render(<App />)
+  const saveButton = queryByTestId('save-btn')
+  fireEvent.click(saveButton, { color: '#112233' })
+  expect(localStorage.getItem('theme')).toBeTruthy()
+})
