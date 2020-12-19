ed0bebbf953f37493cafe6c80b18e2349d6534b3
el mehdi rhindi <g6handi@gmail.com>
2020-12-19 14:12:28 +0100

    added style metadata and regex validation system

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import Section from './Section/Section'
 import React, { useReducer } from 'react'
+import ThemeEditor from './ThemeEditor/ThemeEditor'
+import initialThemeObject from './initialThemeObject'
 import './App.css';
-
-const validationRegex = {
-  color: /^#(?:[0-9a-f]{3}){1,2}$/i,
-  simpleSize: /^([+-]?(?:\d+|\d*\.\d+))(px|em|rem)$/,
-  sizeWithCalc: /^(([+]?(?:\d+|\d*\.\d+))(px|em|rem)|(calc\()((\s+)?)([+]?(?:\d+|\d*\.\d+))(px|em|rem)((\s+)?)(\+|\-)((\s+)?)([+]?(?:\d+|\d*\.\d+))(px|em|rem)((\s+)?)(\))|(calc\()((\s+)?)([+-]?(?:\d+|\d*\.\d+))(px|em|rem)((\s+)?)(\*|\/)((\s+)?)([+]?(?:\d+|\d*\.\d+))((\s+)?)(\)))$/,
-  border: /^([+]?(?:\d+|\d*\.\d+))(px|em|rem)(\s+)(solid|dashed)(\s+)#(?:[0-9a-f]{3}){1,2}$/
-}
-
-const initialTheme = {
-  colors: {
-    primary: { value: '#000000', description: 'Primary font color', regex: validationRegex.color },
-    secondary: { value: '#ffffff', description: 'Secondary font color', regex: validationRegex.color }
-  },
-  sizes: {
-    borderWidth: { value: '1px', description: 'Default border width', regex: validationRegex.simpleSize },
-    text: { value: '1.1em', description: 'Default text size', regex: validationRegex.simpleSize }
-  },
-  buttons: {
-    fontSize: { value: 'calc(1.1rem*1.2)', description: 'Font size', regex: validationRegex.sizeWithCalc }
-  },
-  textfield: {
-    border: { value: '{sizes.borderWidth} solid {colors.secondary}', description: 'Border', regex: validationRegex.border }
-  }
-}
 
 const themeReducer = (state, action) => {
   switch (action.type) {
     case 'UPDATE_THEME':
-      const key = Object.keys(action.payload)[0]
-      const value = action.payload[key]
+      const sectionName = Object.keys(action.payload)[0]
+      const styleValue = action.payload[sectionName]
+      const styleName = Object.keys(styleValue)[0]
       return {
         ...state,
-        [key]: {
-          ...state[key],
-          ...value
+        [sectionName]: {
+          ...state[sectionName],
+          [styleName]: {
+            ...state[sectionName][styleName],
+            ...styleValue[styleName]
+          }
         }
       }
     default:
@@ -44,24 +25,29 @@ const themeReducer = (state, action) => {
 }
 
 function App() {
-  const [themeState, themeDispatch] = useReducer(themeReducer, JSON.parse(localStorage.getItem('theme')) || initialTheme)
-  const saveTheme = () => {
-    localStorage.setItem('theme', JSON.stringify(themeState))
+  const [themeState, themeDispatch] = useReducer(themeReducer, loadTheme())
+  function loadTheme() {
+    let loadedTheme = {}
+    const savedTheme = JSON.parse(localStorage.getItem('theme'))
+    if (!savedTheme) return initialThemeObject
+    Object.entries(initialThemeObject).forEach(section => {
+      Object.entries(section[1]).forEach(style => {
+        loadedTheme = {
+          ...loadedTheme,
+          [section[0]]: {
+            ...loadedTheme[section[0]],
+            [style[0]]: {
+              ...style[1],
+              styleValue: savedTheme[section[0]][style[0]].styleValue,
+              styleValueResolved: savedTheme[section[0]][style[0]].styleValueResolved
+            }
+          }
+        }
+      })
+    })
+    return loadedTheme
   }
-  return (
-    <>
-      <div>
-        {Object.entries(themeState).map((el) => {
-          return <Section
-            key={el[0]}
-            section={el}
-            themeState={themeState}
-            themeDispatch={themeDispatch} />
-        })}
-      </div>
-      <button data-testid='save-btn' onClick={saveTheme}>Save</button>
-    </>
-  );
+  return <ThemeEditor themeState={themeState} themeDispatch={themeDispatch} />
 }
 
 export default App;
--- src/StylePicker/StylePicker.js
+++ src/StylePicker/StylePicker.js
@@ -1,65 +1,118 @@
 import React, { useState, useEffect } from 'react'
 import './StylePicker.css';
 
-const StylePicker = ({ description, styleValue = '', regex, sectionName, styleName, themeState, themeDispatch }) => {
-  const [style, setStyle] = useState(styleValue)
-  const [resolvedStyle, setResolvedStyle] = useState('')
-  const [error, setError] = useState('')
-  const [validationRegex, setValidationRegex] = useState(new RegExp(regex))
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isValid()) {
-      themeDispatch(
-        {
-          type: 'UPDATE_THEME',
-          payload: { [sectionName]: { [styleName]: { value: style, description: description } } }
-        }
-      )
+const StylePicker = ({ sectionName, styleName, themeState, themeDispatch }) => {
+  const styleData = themeState[sectionName][styleName]
+  const [style, setStyle] = useState(themeState[sectionName][styleName].styleValue)
+  const [resolvedStyle, setResolvedStyle] = useState(themeState[sectionName][styleName].styleValueResolved)
+  const [errors, setErrors] = useState([])
+
+  const validateRegex = (value, validationArray) => {
+    let myValue = value.slice()
+    let pos = 0
+
+    for (let i = 0; i < validationArray.length; i++) {
+      if (myValue.match(validationArray[i].regex)) {
+        pos += myValue.match(validationArray[i].regex).length
+        myValue = myValue.replace(validationArray[i].regex, "")
+      } else {
+        return [validationArray[i].message, pos]
+      }
     }
+
+    return ['NO_ERROR', -1]
   }
-  const isValid = () => {
-    setError('')
-    setStyle(currentValue => currentValue && currentValue.trim())
-    if (!style) {
-      setError('a value is required')
+
+  const resolveStyle = (styleVar) => {
+    let containsUndefined = false
+    const metadata = []
+    if (!styleData.metadata.allowVariables) {
+      return [styleVar, metadata, containsUndefined]
+    }
+    if (styleVar && !styleVar.match(/\{(.*?)\}/g)) {
+      return [styleVar, metadata, containsUndefined]
+    }
+    const newResolvedStyle = styleVar && styleVar.replace(/\{(.*?)\}/g, (match) => {
+      const variable = match
+      const propsArray = match.replace('{', '').replace('}', '').split('.')
+      const style = themeState[propsArray[0]]
+        && themeState[propsArray[0]][propsArray[1]]
+        && themeState[propsArray[0]][propsArray[1]].styleValue
+
+      if (style === undefined) {
+        containsUndefined = true
+      }
+
+      metadata.push({ variable, style })
+      return style
+    });
+    return [newResolvedStyle, metadata, containsUndefined]
+  }
+
+  const validateInput = (inputStyle) => {
+    setErrors([])
+    const resolvedStyleData = resolveStyle(style)
+    if (inputStyle === '') {
+      setErrors(['Input cannot be empty'])
       return false
-    } else if (resolveStyle(style).includes('undefined')) {
-      setError('one or more variables do no exist')
+    }
+    if (!styleData.metadata.allowVariables && (inputStyle.includes('{') || inputStyle.includes('}'))) {
+      setErrors(['Variables are not allowed for this style input'])
       return false
-    } else if (!resolveStyle(style).match(validationRegex)) {
-      setError(`invalid input for ${sectionName} ${styleName}`)
+    }
+    if (resolvedStyleData[2]) {
+      const [resolvedStyle, metadata] = resolvedStyleData
+      let errorMessage = 'The following variables do not exist: '
+      metadata.forEach(el => {
+        if (el.style === undefined) {
+          errorMessage += ` ${el.variable}`
+        }
+      })
+      setErrors([errorMessage])
       return false
+    }
+    if (styleData.metadata.validationRegex.length > 0) {
+      const errorMessages = []
+      styleData.metadata.validationRegex.forEach(el => {
+        errorMessages.push(validateRegex(resolvedStyleData[0], el))
+      })
+      const noError = errorMessages.find(el => el[0] === 'NO_ERROR')
+      if (!noError) {
+        errorMessages.forEach(el => {
+          setErrors(previousErrors => [...previousErrors, `${el[0]} at position ${el[1]}`])
+        })
+        return false
+      }
     }
     return true
   }
-  const resolveStyle = (styleVar) => {
-    if (styleVar && !styleVar.match(/\{(.*?)\}/g)) {
-      return styleVar
-    }
-    const newResolvedStyle = styleVar && styleVar.replace(/\{(.*?)\}/g, (match) => {
-      const propsArray = match.replace('{', '').replace('}', '').split('.')
-      return themeState[propsArray[0]]
-        && themeState[propsArray[0]][propsArray[1]]
-        && themeState[propsArray[0]][propsArray[1]].value
-    });
-    return newResolvedStyle
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateInput(style)) return
+    themeDispatch(
+      {
+        type: 'UPDATE_THEME',
+        payload: { [sectionName]: { [styleName]: { styleValue: style, styleValueResolved: resolveStyle(style)[0] } } }
+      }
+    )
   }
+
   useEffect(() => {
-    setResolvedStyle(resolveStyle(style))
+    setResolvedStyle(resolveStyle(style)[0])
   }, [themeState])
-  useEffect(() => {
-    setTimeout(isValid, 500)
-  }, [style])
+
   return (
     <div data-testid='style-picker' className="style-picker-container">
-      <span>{description}: </span>
-      <strong data-testid='resolved-style'>{resolvedStyle}</strong>
+      <span>{styleData.metadata.description}: </span>
+      <strong data-testid='resolved-style'>
+        {resolvedStyle}
+      </strong>
       <i>{sectionName}.{styleName}</i>
       <br />
       <form onSubmit={handleSubmit}>
         <label htmlFor='style'>Value : </label>
         <input
-          required
           type='text'
           id='style'
           name='style'
@@ -67,7 +120,13 @@ const StylePicker = ({ description, styleValue = '', regex, sectionName, styleNa
           value={style}
           onChange={(e) => setStyle(e.target.value)}
         />
-        <p data-testid='error-message'>{error}</p>
+        {
+          !!errors.length &&
+          <ul>
+            Please verify that you respect at least one of the following:
+            {errors.map(error => <li key={error}>{error}</li>)}
+          </ul>
+        }
         <button data-testid="ok-btn" type='submit'>OK</button>
       </form>
     </div>
